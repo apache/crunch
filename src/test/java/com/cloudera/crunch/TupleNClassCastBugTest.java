b9e2270251791f6040ee64b3597703757f4328c2
Elden Bishop <elden.bishop@gmail.com>
2011-10-14 11:11:44 -0700

    Added unit test for the bug fixed in last commit.

---
+++ src/test/java/com/cloudera/crunch/TupleNClassCastBugTest.java
@@ -0,0 +1,11 @@
+package com.cloudera.crunch;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ebishop
+ * Date: 10/14/11
+ * Time: 10:42 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TupleNClassCastBugTest {
+}
