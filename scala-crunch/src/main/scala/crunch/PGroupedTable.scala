bbc8cdbf5fe841d7b92f33ad84bd20a544fd3093
Josh Wills <josh.wills@gmail.com>
2011-10-15 16:07:31 -0700

    implement grouping wrapper fns

--- scala-crunch/src/main/scala/crunch/PGroupedTable.scala
+++ scala-crunch/src/main/scala/crunch/PGroupedTable.scala
@@ -3,7 +3,7 @@ package crunch
 import com.cloudera.crunch.{DoFn, Emitter, FilterFn, MapFn}
 import com.cloudera.crunch.{CombineFn, PGroupedTable => JGroupedTable, PTable => JTable, Pair => JPair}
 import java.lang.{Iterable => JIterable}
-import scala.collection.Iterable
+import scala.collection.{Iterable, Iterator}
 
 class PGroupedTable[K, V](grouped: JGroupedTable[K, V]) extends PCollection[JPair[K, JIterable[V]]](grouped) with JGroupedTable[K, V] {
 
@@ -43,32 +43,47 @@ class PGroupedTable[K, V](grouped: JGroupedTable[K, V]) extends PCollection[JPai
   override def ungroup() = new PTable[K, V](grouped.ungroup())
 }
 
+class ConversionIterator[S](iterator: java.util.Iterator[S]) extends Iterator[S] {
+  override def hasNext(): Boolean = iterator.hasNext()
+
+  override def next(): S = Conversions.c2s(iterator.next()).asInstanceOf[S]
+}
+
+class ConversionIterable[S](iterable: JIterable[S]) extends Iterable[S] {
+  override def iterator() = new ConversionIterator[S](iterable.iterator())
+}
+
 class SGroupedTableFilterFn[K, V](f: (Any, Iterable[Any]) => Boolean) extends FilterFn[JPair[K, JIterable[V]]] {
   override def accept(input: JPair[K, JIterable[V]]): Boolean = {
-    //f(Conversions.c2s(input.first()), Conversions.c2s(input.second()))
-    false
+    f(Conversions.c2s(input.first()), new ConversionIterable[V](input.second()))
   }
 }
 
 class SGroupedTableDoFn[K, V, T](fn: (Any, Iterable[Any]) => Seq[T]) extends DoFn[JPair[K, JIterable[V]], T] {
   override def process(input: JPair[K, JIterable[V]], emitter: Emitter[T]): Unit = {
-    
+    for (v <- fn(Conversions.c2s(input.first()), new ConversionIterable[V](input.second()))) {
+      emitter.emit(Conversions.s2c(v).asInstanceOf[T])
+    }
   }
 }
 
 class SGroupedTableDoTableFn[K, V, L, W](fn: (Any, Iterable[Any]) => Seq[(Any, Any)]) extends DoFn[JPair[K, JIterable[V]], JPair[L, W]] {
   override def process(input: JPair[K, JIterable[V]], emitter: Emitter[JPair[L, W]]): Unit = {
+    for ((f, s) <- fn(Conversions.c2s(input.first()), new ConversionIterable[V](input.second()))) {
+      emitter.emit(JPair.of(Conversions.s2c(f).asInstanceOf[L], Conversions.s2c(s).asInstanceOf[W]))
+    }
   }
 }
 
 class SGroupedTableMapFn[K, V, T](fn: (Any, Iterable[Any]) => T) extends MapFn[JPair[K, JIterable[V]], T] {
   override def map(input: JPair[K, JIterable[V]]): T = {
-    null.asInstanceOf[T]
+    Conversions.s2c(fn(Conversions.c2s(input.first()), new ConversionIterable[V](input.second()))).asInstanceOf[T]
   }
 }
 
 class SGroupedTableMapTableFn[K, V, L, W](fn: (Any, Iterable[Any]) => (Any, Any)) extends MapFn[JPair[K, JIterable[V]], JPair[L, W]] {
   override def map(input: JPair[K, JIterable[V]]): JPair[L, W] = {
-    null.asInstanceOf[JPair[L, W]]
+    val (f, s) = fn(Conversions.c2s(input.first()), new ConversionIterable[V](input.second()))
+    JPair.of(Conversions.s2c(f).asInstanceOf[L], Conversions.s2c(s).asInstanceOf[W])
   }
 }
