bf58906b1e365cb7c14c6c046ac550e87cde340b
Gabriel Reid <greid@apache.org>
2014-02-15 18:05:34 +0100

    CRUNCH-344 URL-encode paths in CrunchInputs
    Use URL-encoding on paths when serializing input information to
    the Configuration to avoid issues with paths that contain the
    separator characters used for separating the serialized structure
    stored in the Configuration.

--- crunch-core/src/main/java/org/apache/crunch/io/CrunchInputs.java
+++ crunch-core/src/main/java/org/apache/crunch/io/CrunchInputs.java
@@ -17,23 +17,25 @@
  */
 package org.apache.crunch.io;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import com.google.common.base.Joiner;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.InputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.JobContext;
-
-import com.google.common.base.Joiner;
-import com.google.common.base.Preconditions;
-import com.google.common.base.Splitter;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 
 /**
  * Helper functions for configuring multiple {@code InputFormat} instances within a single
@@ -54,13 +56,13 @@ public class CrunchInputs {
 
   public static void addInputPaths(Job job, Collection<Path> paths, FormatBundle inputBundle, int nodeIndex) {
     Configuration conf = job.getConfiguration();
-    List<String> pathStrs = Lists.newArrayListWithExpectedSize(paths.size());
+    List<String> encodedPathStrs = Lists.newArrayListWithExpectedSize(paths.size());
     for (Path path : paths) {
-      String pathStr = path.toString();
+      String pathStr = encodePath(path);
       Preconditions.checkArgument(pathStr.indexOf(RECORD_SEP) == -1 && pathStr.indexOf(FIELD_SEP) == -1 && pathStr.indexOf(PATH_SEP) == -1);
-      pathStrs.add(pathStr);
+      encodedPathStrs.add(pathStr);
     }
-    String inputs = JOINER.join(inputBundle.serialize(), String.valueOf(nodeIndex), Joiner.on(PATH_SEP).join(pathStrs));
+    String inputs = JOINER.join(inputBundle.serialize(), String.valueOf(nodeIndex), Joiner.on(PATH_SEP).join(encodedPathStrs));
     String existing = conf.get(CRUNCH_INPUTS);
     conf.set(CRUNCH_INPUTS, existing == null ? inputs : existing + RECORD_SEP + inputs);
   }
@@ -76,19 +78,35 @@ public class CrunchInputs {
       List<String> fields = Lists.newArrayList(SPLITTER.split(input));
       FormatBundle<InputFormat> inputBundle = FormatBundle.fromSerialized(fields.get(0), job.getConfiguration());
       if (!formatNodeMap.containsKey(inputBundle)) {
-        formatNodeMap.put(inputBundle, Maps.<Integer, List<Path>> newHashMap());
+        formatNodeMap.put(inputBundle, Maps.<Integer, List<Path>>newHashMap());
       }
       Integer nodeIndex = Integer.valueOf(fields.get(1));
       if (!formatNodeMap.get(inputBundle).containsKey(nodeIndex)) {
-        formatNodeMap.get(inputBundle).put(nodeIndex, Lists.<Path> newLinkedList());
+        formatNodeMap.get(inputBundle).put(nodeIndex, Lists.<Path>newLinkedList());
       }
       List<Path> formatNodePaths = formatNodeMap.get(inputBundle).get(nodeIndex);
-      String paths = fields.get(2);
-      for (String path : Splitter.on(PATH_SEP).split(paths)) {
-        formatNodePaths.add(new Path(path));
+      String encodedPaths = fields.get(2);
+      for (String encodedPath : Splitter.on(PATH_SEP).split(encodedPaths)) {
+        formatNodePaths.add(decodePath(encodedPath));
       }
     }
     return formatNodeMap;
+  }
+
+  private static String encodePath(Path path) {
+    try {
+      return URLEncoder.encode(path.toString(), "UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static Path decodePath(String encodedPath) {
+    try {
+      return new Path(URLDecoder.decode(encodedPath, "UTF-8"));
+    } catch (UnsupportedEncodingException e) {
+      throw new RuntimeException(e);
+    }
   }
 
 }
